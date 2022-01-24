1ef23d1385a0bf9bd401d1473972851727cdebd7
Callum/Xavi <callummander@gmail.com>
2022-01-24 16:48:55 +0000

    added components file and Header.jsx file

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    
+    return (
+        <header className="header">
+            <h1>I'm Callum.</h1>
+            <h2>A Developer.</h2>
+        </header>
+    )
+}
