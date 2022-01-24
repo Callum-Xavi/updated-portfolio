e6cbf44f117bb081440062635e42cb4d33a51cb8
Callum/Xavi <callummander@gmail.com>
2022-01-24 17:11:23 +0000

    added navbar and header code from original project - styling not complete as yet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,23 @@
 export const Header = () => {
     
     return (
-        <header className="header">
-            <h1>I'm Callum.</h1>
-            <h2>A Developer.</h2>
+
+        <header class="header h-auto bg-blue-400">
+            <nav class="nav-section flex justify-center md:justify-end pt-20 pr-10">
+                <a class="nav-link" href="#">About me</a>
+                <a class="nav-link" href="#">My skills</a>
+                <a class="nav-link" href="#">My work</a>
+                <a class="nav-link" href="#">Contact</a>
+            </nav>
+
+            <section class="title mt-10">
+                <h1>I'm Callum.</h1>
+                <h2>a Developer.</h2>
+            </section>
+
+            {/* <!-- <img class="h-32 w-32" src="images/ladderAnimation.png" alt="still animated person climbing ladder"> --> */}
+            <img class="h-64 w-64 ml-20" src="images/templeJapan.png" alt="Japanese Temple"/>
+            
         </header>
     )
 }
