f8e522280791127208aa5271fa5b406caf2e51bb
Callum/Xavi <callummander@gmail.com>
2022-01-25 18:09:29 +0000

    renamed all instances of 'class' to react 'className'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,24 @@
+import "./header.css";
 
 export const Header = () => {
     
     return (
 
-        <header class="header h-auto bg-blue-400">
-            <nav class="nav-section flex justify-center md:justify-end pt-20 pr-10">
-                <a class="nav-link" href="#">About me</a>
-                <a class="nav-link" href="#">My skills</a>
-                <a class="nav-link" href="#">My work</a>
-                <a class="nav-link" href="#">Contact</a>
+        <header className="header h-auto bg-blue-400">
+            <nav className="nav-section flex justify-center md:justify-end pt-20 pr-10">
+                <a className="nav-link" href="#">About me</a>
+                <a className="nav-link" href="#">My skills</a>
+                <a className="nav-link" href="#">My work</a>
+                <a className="nav-link" href="#">Contact</a>
             </nav>
 
-            <section class="title mt-10">
+            <section className="title mt-10">
                 <h1>I'm Callum.</h1>
                 <h2>a Developer.</h2>
             </section>
 
             {/* <!-- <img class="h-32 w-32" src="images/ladderAnimation.png" alt="still animated person climbing ladder"> --> */}
-            <img class="h-64 w-64 ml-20" src="images/templeJapan.png" alt="Japanese Temple"/>
+            <img className="h-64 w-64 ml-20" src="images/templeJapan.png" alt="Japanese Temple"/>
             
         </header>
     )
